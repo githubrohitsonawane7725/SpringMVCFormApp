4c3e8b1b533f29b26f9bb700b349648083f357f9
Rohit sonawane <114295234+rohit772525@users.noreply.github.com>
2024-04-07 13:05:39 +0530

    Update UserController.java

--- src/main/java/com/ashok/controller/UserController.java
+++ src/main/java/com/ashok/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.ashok.model.User;
+
 
 @Controller
 public class UserController {
