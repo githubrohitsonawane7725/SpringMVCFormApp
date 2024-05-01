5cf57ccbcfab45d85e4d2e1b0e042fcc747475ca
Rohit sonawane <114295234+rohitsonawane123@users.noreply.github.com>
2024-05-01 12:29:30 +0530

    Update UserController.java

--- src/main/java/com/ashok/controller/UserController.java
+++ src/main/java/com/ashok/controller/UserController.java
@@ -1,29 +1,27 @@
+// UserController.java
 package com.ashok.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.ashok.model.User;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping(value = "/displayForm.htm", method = RequestMethod.GET)
+    @GetMapping("/form")
     public String displayForm(Model model) {
-   
-        
-        
         model.addAttribute("user", new User());
         return "userForm";
     }
 
-    @RequestMapping(value = "/regUser.htm", method = RequestMethod.POST)
-    public String regUser(Model model, @ModelAttribute("user") User u) {
-        model.addAttribute("user", u);
+    @PostMapping("/register")
+    public String registerUser(Model model, User user) {
+        model.addAttribute("user", user);
         return "display";
     }
-
 }
+
