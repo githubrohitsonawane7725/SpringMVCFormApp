ef51d05fe214fbe6067d87dad71a7d5125fd961f
Rohit sonawane <114295234+rohitsonawane123@users.noreply.github.com>
2024-05-01 12:20:44 +0530

    Update UserController.java

--- src/main/java/com/ashok/controller/UserController.java
+++ src/main/java/com/ashok/controller/UserController.java
@@ -2,28 +2,25 @@ package com.ashok.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.ashok.model.User;
 
 @Controller
 public class UserController {
 
-    @RequestMapping(value = "/displayForm.htm", method = RequestMethod.GET)
-    public String displayForm(Model model) {
-   
-        
-        
+    @GetMapping("/registration")
+    public String displayRegistrationForm(Model model) {
         model.addAttribute("user", new User());
-        return "userForm";
+        return "registrationForm";
     }
 
-    @RequestMapping(value = "/regUser.htm", method = RequestMethod.POST)
-    public String regUser(Model model, @ModelAttribute("user") User u) {
-        model.addAttribute("user", u);
-        return "display";
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") User user, Model model) {
+        // Here, you can add validation logic if needed
+        model.addAttribute("user", user);
+        return "registrationConfirmation";
     }
-
 }
