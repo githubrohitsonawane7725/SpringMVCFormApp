2765b7f76e269f458ab4dc2b2696b090c36ee8c4
Rohit sonawane <114295234+rohitsonawane123@users.noreply.github.com>
2024-05-01 12:43:06 +0530

    Update User.java

--- src/main/java/com/ashok/model/User.java
+++ src/main/java/com/ashok/model/User.java
@@ -1,19 +1,41 @@
-// User.java
 package com.ashok.model;
 
 public class User {
 
-    private String username;
-    private String password;
-    private String email;
+	private String uname;
+	private String pwd;
+	private String email;
+	private Long phno;
+	private String gender;
 
-    // Constructors, getters, and setters
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [uname=" + uname + ", pwd=" + pwd + ", email=" + email + "]";
+	}
+
 }
